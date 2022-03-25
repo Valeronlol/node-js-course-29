16dca1385981ade0c67d9f605f19a67cc440ebdc
Rostislav Radionov <rostislav.radionov018@gmail.com>
2022-03-25 18:30:05 +0300

    added: fix

--- details/car.js
+++ details/car.js
@@ -1,27 +1,23 @@
-const CarBody = require('./car-body')
-const Engine = require('./engine')
+module.exports = class Car {
+  constructor(carBody, engine) {
+    this.carBody = carBody;
+    this.engine = engine;
+  }
 
-class Car {
-    constructor () {
-        this.carBody = new CarBody()
-        this.engine = new Engine()
+  create() {
+    const parts = this.carBody.install();
+
+    this.engine.install(parts);
+
+    return this;
+  }
+
+  run() {
+    if (!this.carBody || !this.engine) {
+      throw new Error("Can't run car with non installed parts.");
     }
 
-    create () {
-        this.carBody.install()
-        this.engine.install()
-
-        return this
-    }
-
-    run () {
-        if (!this.carBody || !this.engine) {
-            throw new Error('Can\'t run car with non installed parts.')
-        }
-
-        this.engine.run()
-        console.log('The car started, good luck!')
-    }
-}
-
-module.exports = Car
+    this.engine.run();
+    console.log("The car started, good luck!");
+  }
+};
